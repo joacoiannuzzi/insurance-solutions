2d25dae9cdb57e5caf9b44e84ed6423736ee6f8a
Sebas Belaustegui <sebasbelaustegui@hotmail.com>
2020-08-09 22:56:02 -0300

    FE list example

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,17 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {UserService} from "../shared/services/user.service";
+import { UserListComponent } from './user-list/user-list.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
