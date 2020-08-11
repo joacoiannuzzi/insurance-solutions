eafe0368ee599e55b7c51414a763e09b9931303d
tomaslopez <tomy.lopez1999@gmail.com>
2020-08-11 19:22:36 -0300

    Swagger implemented.

--- src/main/java/com/insurance/solutions/app/InsuranceSolutionsApplication.java
+++ src/main/java/com/insurance/solutions/app/InsuranceSolutionsApplication.java
@@ -1,16 +1,28 @@
 package com.insurance.solutions.app;
 
+import com.google.common.collect.Lists;
 import com.insurance.solutions.app.models.User;
 import com.insurance.solutions.app.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.stream.Stream;
 
 @SpringBootApplication
+@EnableSwagger2
 public class InsuranceSolutionsApplication {
 
     public static void main(String[] args) {
@@ -27,6 +39,37 @@ public class InsuranceSolutionsApplication {
                 });
             }
         };
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .securitySchemes(Lists.newArrayList(apiKey()))
+                .securityContexts(Lists.newArrayList(securityContext()));
+    }
+    @Bean
+    SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(
+                new SecurityReference("JWT", authorizationScopes));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
     }
 
 }
