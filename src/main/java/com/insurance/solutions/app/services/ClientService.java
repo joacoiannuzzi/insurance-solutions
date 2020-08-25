b861e15733e2b2b95f11d2e41c0eb54bb4e7ae4d
joacoiannuzzi <joaco.iannuzzi@gmail.com>
2020-08-25 20:30:29 -0300

    merged dev

--- src/main/java/com/insurance/solutions/app/services/ClientService.java
+++ src/main/java/com/insurance/solutions/app/services/ClientService.java
@@ -29,7 +29,5 @@ public class ClientService {
                     return ResponseEntity.ok().build();
                 })
                 .orElseThrow(() -> new BadRequestException("El cliente con id: " + id + " no existe."));
-
-
     }
 }
