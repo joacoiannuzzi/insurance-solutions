9591e04bb95b6138d99763b8794c94ee2f0084ab
Alejo Ramirez Gismondi <28781872+Alejo-RamirezGismondi@users.noreply.github.com>
2020-09-08 17:04:36 -0300

    created Vehicle class

---
+++ web/src/shared/models/vehicle.ts
@@ -0,0 +1,24 @@
+export class Vehicle {
+  id: number;
+  licensePlate: string;
+  category: string;
+  brand: string;
+  model: string;
+  drivingProfiles: string;
+  monitoringSystems: string;
+
+
+  constructor(id: number, licensePlate: string, category: string, brand: string, model: string, drivingProfiles: string, monitoringSystems: string) {
+    this.id = id;
+    this.licensePlate = licensePlate;
+    this.category = category;
+    this.brand = brand;
+    this.model = model;
+    this.drivingProfiles = drivingProfiles;
+    this.monitoringSystems = monitoringSystems;
+  }
+
+  static fromJsonObject(jsonObject: any): Vehicle {
+    return new Vehicle(jsonObject.id, jsonObject.licensePlate, jsonObject.category, jsonObject.brand, jsonObject.model, jsonObject.drivingProfiles, jsonObject.monitoringSystems);
+  }
+}
