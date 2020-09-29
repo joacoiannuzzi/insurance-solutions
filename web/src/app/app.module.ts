55f910de4a9ded8acb5ffc3e0f90d850e531270f
Alejo Ramirez Gismondi <28781872+Alejo-RamirezGismondi@users.noreply.github.com>
2020-09-29 17:11:48 -0300

    created monitoring-system package and details component

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -35,6 +35,7 @@ import {MatSelectModule} from "@angular/material/select";
 import { DrivingProfilesComponent } from './pages/vehicle/driving-profiles/driving-profiles.component';
 import { VehicleDetailsComponent } from './pages/vehicle/vehicle-details/vehicle-details.component';
 import { VehicleUpdateComponent } from './pages/vehicle/vehicle-update/vehicle-update.component';
+import { MonitoringSystemDetailsComponent } from './pages/monitoring-system/monitoring-system-details/monitoring-system-details.component';
 
 @NgModule({
   declarations: [
@@ -53,7 +54,8 @@ import { VehicleUpdateComponent } from './pages/vehicle/vehicle-update/vehicle-u
     DrivingProfilesComponent,
     VehicleAddComponent,
     VehicleDetailsComponent,
-    VehicleUpdateComponent
+    VehicleUpdateComponent,
+    MonitoringSystemDetailsComponent
    ],
   imports: [
     BrowserModule,
---
+++ web/src/app/pages/monitoring-system/monitoring-system-details/monitoring-system-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-monitoring-system-details',
+  templateUrl: './monitoring-system-details.component.html',
+  styleUrls: ['./monitoring-system-details.component.scss']
+})
+export class MonitoringSystemDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
