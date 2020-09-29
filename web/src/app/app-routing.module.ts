e6570b7dedc32ae64448b8d22619af5525a61f73
Diego Alvarado <diegoalvarado26@hotmail.com>
2020-09-29 12:04:26 -0300

    added the monitoring systems list

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { MonitoringSystemListComponent } from './pages/monitoring-system/monitoring-system-list/monitoring-system-list.component';
 import { VehicleListComponent } from './pages/vehicle/vehicle-list/vehicle-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -11,7 +12,13 @@ const routes: Routes = [
   {
     path: 'vehicles', children: [
       { path: '', component: VehicleListComponent }
-  ]}
+    ]
+  },
+  {
+    path: 'monitoring-systems', children: [
+      { path: '', component: MonitoringSystemListComponent}
+    ]
+  }
 ];
 
 @NgModule({
---
+++ web/src/shared/services/monitoring-system.service.ts
@@ -0,0 +1,47 @@
+import { MonitoringSystem } from './../models/monitoringSystem';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Client} from '../models/client';
+import {Observable} from "rxjs";
+import {catchError, map} from "rxjs/operators";
+import {Vehicle} from "../models/vehicle";
+import {MatSnackBar} from "@angular/material/snack-bar";
+
+@Injectable()
+export class MonitoringSystemService {
+
+  private readonly monitoringSystemsUrl: string;
+  private monitoringSystemsList: MonitoringSystem[];
+
+  constructor(private http: HttpClient, private snackBar: MatSnackBar) {
+    this.monitoringSystemsUrl = 'http://localhost:8080/monitoring-systems';
+  }
+
+  private findAll(): Observable<MonitoringSystem[]> {
+    return this.http.get(this.monitoringSystemsUrl + "/get-all").pipe(
+      map((res: any) => {
+        this.monitoringSystemsList = res.map((monitoringSystem) => MonitoringSystem.fromJsonObject(monitoringSystem));
+        return this.monitoringSystemsList;
+      }),
+      catchError(() => {
+        this.snackBar.open('Hubo un error al traer los servicios de monitoreo..', '', {
+          duration: 2000,
+        });
+        return this.monitoringSystems;
+      })
+    );
+  }
+ 
+
+  
+  get monitoringSystems(): Observable<MonitoringSystem[]> {
+    return this.monitoringSystemsList
+      ? new Observable<MonitoringSystem[]>((subscriber) =>
+        subscriber.next(this.monitoringSystemsList)
+      )
+      : this.findAll();
+  }
+
+
+  
+}
