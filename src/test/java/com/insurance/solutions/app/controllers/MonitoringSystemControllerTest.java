8bb46d989e2b2c49df3a6f96b57289dbe72a38c1
Tomas Lopez <tomy.lopez1999@gmail.com>
2020-10-01 10:44:09 -0300

    Feature/be us 014 (#63)
    * Delete monitoring system
    
    * Delete monitoring system test

--- src/test/java/com/insurance/solutions/app/controllers/MonitoringSystemControllerTest.java
+++ src/test/java/com/insurance/solutions/app/controllers/MonitoringSystemControllerTest.java
@@ -6,6 +6,7 @@ import com.insurance.solutions.app.models.MonitoringSystem;
 import com.insurance.solutions.app.repositories.MonitoringSystemRepository;
 import com.insurance.solutions.app.exceptions.ResourceNotFoundException;
 import com.insurance.solutions.app.services.MonitoringSystemService;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -21,8 +22,8 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -160,5 +161,48 @@ class MonitoringSystemControllerTest {
 
         assertEquals("Size should be the same", all.size(), list.size());
 
+    }
+
+    @Test
+    public void deleteExistingMonitoringSystem() throws Exception {
+        MonitoringSystem monitoringSystem = new MonitoringSystem("name2", "sensor2", "monitoringCompany2");
+
+        long monitoringSystemId = monitoringSystemService.createMonitoringSystem(monitoringSystem).getId();
+
+
+        Assert.assertEquals(toJson(monitoringSystem), toJson(monitoringSystemService.findById(monitoringSystemId)));
+
+        mockMvc
+                .perform(
+                        delete(urlBase + "/delete/" + monitoringSystemId)
+                )
+                .andExpect(status().isOk());
+
+        Exception exception = Assert.assertThrows(ResourceNotFoundException.class, () -> monitoringSystemService.findById(monitoringSystemId));
+        Assert.assertEquals("Monitoring system not found.", exception.getMessage());
+
+        mockMvc
+                .perform(
+                        delete(urlBase + "/delete/" + monitoringSystemId)
+                )
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void deleteNotExistingMonitoringSystem() throws Exception {
+
+        List<MonitoringSystem> before = monitoringSystemService.findAll();
+
+        long idToDelete = 57775786867867878L;
+
+        mockMvc
+                .perform(
+                        delete(urlBase + "/delete/" + idToDelete)
+                )
+                .andExpect(status().isNotFound());
+
+        List<MonitoringSystem> after = monitoringSystemService.findAll();
+
+        assertEquals("Size should be the same", before.size(), after.size());
     }
 }
