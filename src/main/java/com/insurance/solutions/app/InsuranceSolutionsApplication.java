e332e561377b07a389a848128acec7b9ceda53da
JoseRojas96 <jose.rojas@ing.austral.edu.ar>
2020-10-09 15:30:53 -0300

    Merge branch dev with this -> added mock data to db

--- src/main/java/com/insurance/solutions/app/InsuranceSolutionsApplication.java
+++ src/main/java/com/insurance/solutions/app/InsuranceSolutionsApplication.java
@@ -1,14 +1,10 @@
 package com.insurance.solutions.app;
 
-import com.insurance.solutions.app.models.User;
-import com.insurance.solutions.app.repositories.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import java.util.stream.Stream;
 
 @SpringBootApplication
 public class InsuranceSolutionsApplication {
@@ -20,17 +16,5 @@ public class InsuranceSolutionsApplication {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    CommandLineRunner init(UserRepository userRepository) {
-        return args -> {
-            if (!userRepository.findAll().iterator().hasNext()) {
-                Stream.of("Sebastian", "Tomas", "Franco", "Jose").forEach(name -> {
-                    User user = new User(name, name.toLowerCase() + "@mail.com", new BCryptPasswordEncoder().encode("password"));
-                    userRepository.save(user);
-                });
-            }
-        };
     }
 }
