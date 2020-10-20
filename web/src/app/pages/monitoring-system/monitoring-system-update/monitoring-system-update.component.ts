b353915896c23efcc5164ed1feea5e36b5b2fd37
Alejo Ramirez Gismondi <28781872+Alejo-RamirezGismondi@users.noreply.github.com>
2020-10-20 18:37:45 -0300

    Added custom message for the html in the add component validator. Also added the custom validator for the update component.

--- web/src/app/pages/monitoring-system/monitoring-system-update/monitoring-system-update.component.ts
+++ web/src/app/pages/monitoring-system/monitoring-system-update/monitoring-system-update.component.ts
@@ -1,11 +1,11 @@
-import { MonitoringSystemService } from '../../../../shared/services/monitoring-system.service';
-import { MonitoringSystem } from '../../../../shared/models/monitoringSystem';
+import {MonitoringSystemService} from '../../../../shared/services/monitoring-system.service';
+import {MonitoringSystem} from '../../../../shared/models/monitoringSystem';
 import {Component, Inject, OnInit} from '@angular/core';
 import {
   MatDialogRef,
   MAT_DIALOG_DATA,
 } from '@angular/material/dialog';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AbstractControl, FormControl, FormGroup, ValidatorFn, Validators} from "@angular/forms";
 
 
 @Component({
@@ -17,22 +17,35 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 export class MonitoringSystemUpdateComponent implements OnInit {
   moSys: MonitoringSystem;
   monitoringSystemForm: FormGroup;
+  monitoringSystemNames: MonitoringSystem[] = [];
 
   constructor(
     public dialogRef: MatDialogRef<MonitoringSystemUpdateComponent>,
     @Inject(MAT_DIALOG_DATA) public data: MonitoringSystem,
     public monitoringSystemService: MonitoringSystemService,
   ) {
-    this.moSys = { ...data };
-   }
+    this.moSys = {...data};
+  }
 
 
   ngOnInit() {
+    this.getNames();
+
+    function nameExistsValidator(names: MonitoringSystem[]): ValidatorFn {
+      return (control: AbstractControl): { [key: string]: any } | null => {
+        if (names.find(l => control.value === l.name)) {
+          return {'nameExistsValidator': {value: control.value}}
+        }
+        return null;
+      };
+    }
+
     this.monitoringSystemForm = new FormGroup({
       name: new FormControl(this.moSys.name, [
         Validators.required,
         Validators.minLength(2),
-        Validators.pattern('^[a-zA-Z ]*$')
+        Validators.pattern('^[a-zA-Z ]*$'),
+        nameExistsValidator(this.monitoringSystemNames)
       ]),
       sensor: new FormControl(this.moSys.sensor, [
         Validators.required,
@@ -47,17 +60,35 @@ export class MonitoringSystemUpdateComponent implements OnInit {
 
     })
   }
-  get name() { return this.monitoringSystemForm.get('name');}
-  get sensor() { return this.monitoringSystemForm.get('sensor');}
-  get monitoringCompany() { return this.monitoringSystemForm.get('monitoringCompany');}
-  get invalid() { return this.monitoringSystemForm.invalid }
+
+  private getNames() {
+    this.monitoringSystemService.monitoringSystems.subscribe((res) => {
+      this.monitoringSystemNames = res;
+    })
+  }
+
+  get name() {
+    return this.monitoringSystemForm.get('name');
+  }
+
+  get sensor() {
+    return this.monitoringSystemForm.get('sensor');
+  }
+
+  get monitoringCompany() {
+    return this.monitoringSystemForm.get('monitoringCompany');
+  }
+
+  get invalid() {
+    return this.monitoringSystemForm.invalid
+  }
 
   close(): void {
     this.dialogRef.close();
   }
 
   updateMonitoringSystem() {
-    if (this.monitoringSystemForm.valid){
+    if (this.monitoringSystemForm.valid) {
       Object.keys(this.monitoringSystemForm.value).map((key) => this.data[key] = this.monitoringSystemForm.value[key]);
       this.monitoringSystemService.update(this.data).subscribe(res => {
         this.dialogRef.close(res);
