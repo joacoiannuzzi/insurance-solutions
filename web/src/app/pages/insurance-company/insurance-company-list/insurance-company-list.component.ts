141fc3ad9333088b100f55ada9b767b804e566a4
Alejo Ramirez Gismondi <28781872+Alejo-RamirezGismondi@users.noreply.github.com>
2020-10-20 19:23:44 -0300

    Changed end-point to real one. Tested. Everything seems to work as intended.

--- web/src/app/pages/insurance-company/insurance-company-list/insurance-company-list.component.ts
+++ web/src/app/pages/insurance-company/insurance-company-list/insurance-company-list.component.ts
@@ -6,6 +6,7 @@ import {InsuranceCompanyService} from "../../../../shared/services/insurance-com
 import {MatTableDataSource} from "@angular/material/table";
 import {InsuranceCompany} from "../../../../shared/models/insuranceCompany";
 import {InsuranceCompanyAddComponent} from "../insurance-company-add/insurance-company-add.component";
+import {ConfirmDialogComponent} from "../../../components/confirm-dialog/confirm-dialog.component";
 
 @Component({
   selector: 'app-insurance-company-list',
@@ -64,7 +65,7 @@ export class InsuranceCompanyListComponent implements OnInit, AfterViewInit {
   }
 
   deleteInsuranceCompany(insuranceCompany: InsuranceCompany) {
-    /*this.dialog.open(ConfirmDialogComponent, {
+    this.dialog.open(ConfirmDialogComponent, {
       data: "¿Está seguro de que desea eliminar a la empresa aseguradora " + insuranceCompany.name + "?"
     })
       .afterClosed()
@@ -74,7 +75,7 @@ export class InsuranceCompanyListComponent implements OnInit, AfterViewInit {
             this.getInsuranceCompanies();
           });
         }
-      })*/
+      })
   }
 
   updateInsuranceCompany(insuranceCompany: InsuranceCompany) {
