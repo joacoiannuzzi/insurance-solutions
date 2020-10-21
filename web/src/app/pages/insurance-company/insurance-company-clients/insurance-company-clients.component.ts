6fcdd5bbf345c2e6f30e4beb6efadaba2a169f4a
Alejo Ramirez Gismondi <28781872+Alejo-RamirezGismondi@users.noreply.github.com>
2020-10-20 21:15:08 -0300

    Added all features except for unasigning client. Changed the column name into firstName and lastName

--- web/src/app/pages/insurance-company/insurance-company-clients/insurance-company-clients.component.ts
+++ web/src/app/pages/insurance-company/insurance-company-clients/insurance-company-clients.component.ts
@@ -4,10 +4,11 @@ import {Client} from "../../../../shared/models/client";
 import {MatSort} from "@angular/material/sort";
 import {MatPaginator} from "@angular/material/paginator";
 import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from "@angular/material/dialog";
-import {ClientService} from "../../../../shared/services/client.service";
 import {InsuranceCompany} from "../../../../shared/models/insuranceCompany";
 import {InsuranceCompanyService} from "../../../../shared/services/insurance-company.service";
 import {ConfirmDialogComponent} from "../../../components/confirm-dialog/confirm-dialog.component";
+import {ClientDetailsComponent} from "../../client/client-details/client-details.component";
+import {ClientUpdateComponent} from "../../client/client-update/client-update.component";
 
 @Component({
   selector: 'app-insurance-company-clients',
@@ -15,9 +16,8 @@ import {ConfirmDialogComponent} from "../../../components/confirm-dialog/confirm
   styleUrls: ['./insurance-company-clients.component.scss']
 })
 export class InsuranceCompanyClientsComponent implements OnInit, AfterViewInit {
-  displayedColumns: string[] = ['name', 'dni', 'options'];
+  displayedColumns: string[] = ['firstName', 'lastName', 'dni', 'options'];
   dataSource: MatTableDataSource<Client> = new MatTableDataSource<Client>([]);
-  loading: boolean = true;
 
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
@@ -25,12 +25,12 @@ export class InsuranceCompanyClientsComponent implements OnInit, AfterViewInit {
   constructor(public dialogRef: MatDialogRef<InsuranceCompanyClientsComponent>,
               @Inject(MAT_DIALOG_DATA) public insuranceCompany: InsuranceCompany,
               public dialog: MatDialog,
-              private clientService: ClientService,
               private insuranceCompanyService: InsuranceCompanyService) {
 
   }
 
   ngOnInit(): void {
+    this.dataSource.data = this.insuranceCompany.clients;
     this.paginator._intl.itemsPerPageLabel = 'Elementos por página';
   }
 
@@ -43,6 +43,12 @@ export class InsuranceCompanyClientsComponent implements OnInit, AfterViewInit {
     this.dialogRef.close();
   }
 
+  refreshInsuranceCompany() {
+    this.insuranceCompanyService.insuranceCompanies.subscribe(data => {
+      this.insuranceCompany = data.find(is => is.id === this.insuranceCompany.id);
+    })
+  }
+
   deleteClient(element: Client) {
     this.dialog.open(ConfirmDialogComponent, {
       width: '800px',
@@ -51,7 +57,7 @@ export class InsuranceCompanyClientsComponent implements OnInit, AfterViewInit {
       .afterClosed()
       .subscribe((confirmed: Boolean) => {
         if (confirmed) {
-          this.insuranceCompany.clients.splice()
+          this.insuranceCompany.clients.splice(this.insuranceCompany.clients.findIndex(c => c.dni === element.dni), 1);
           this.insuranceCompanyService.update(this.insuranceCompany).subscribe((res) => {
             if (res) {
 
@@ -61,4 +67,34 @@ export class InsuranceCompanyClientsComponent implements OnInit, AfterViewInit {
       });
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  openClientDetails(element: Client) {
+    const dialogRef = this.dialog.open(ClientDetailsComponent, {
+      width: '800px',
+      data: element
+    });
+    dialogRef.afterClosed().subscribe(() => {
+      this.refreshInsuranceCompany();
+    })
+  }
+
+  updateClient(element: Client) {
+    const dialogRef = this.dialog.open(ClientUpdateComponent, {
+      width: '800px',
+      data: element
+    });
+    dialogRef.afterClosed().subscribe((res) => {
+      if (res) {
+        this.refreshInsuranceCompany();
+      }
+    })
+  }
 }
