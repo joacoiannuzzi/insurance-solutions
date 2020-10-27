39e99a81c41ac91d972c5f324bef6ac6f4c504bf
Alejo Ramirez Gismondi <28781872+Alejo-RamirezGismondi@users.noreply.github.com>
2020-10-27 16:43:32 -0300

    Created User model

---
+++ web/src/shared/models/user.ts
@@ -0,0 +1,22 @@
+import {InsuranceCompany} from "./insuranceCompany";
+
+export class User {
+  id: number;
+  username: string;
+  password: string;
+  type: string;
+  company: InsuranceCompany;
+
+
+  constructor(id: number, username: string, password: string, type: string, company: InsuranceCompany) {
+    this.id = id;
+    this.username = username;
+    this.password = password;
+    this.type = type;
+    this.company = company;
+  }
+
+  static fromJsonToObject(jsonObject: any): User {
+    return new User(jsonObject.id, jsonObject.username, jsonObject.password, jsonObject.type, jsonObject.insuranceCompany);
+  }
+}
