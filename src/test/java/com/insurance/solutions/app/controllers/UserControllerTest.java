dde9fd1ff325a34a5395f6a0188c6c61be7622a4
tomaslopez <tomy.lopez1999@gmail.com>
2020-10-27 19:54:04 -0300

    Create user tests

--- src/test/java/com/insurance/solutions/app/controllers/UserControllerTest.java
+++ src/test/java/com/insurance/solutions/app/controllers/UserControllerTest.java
@@ -1,29 +1,93 @@
 package com.insurance.solutions.app.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.insurance.solutions.app.models.User;
+import com.insurance.solutions.app.models.enums.UserType;
+import com.insurance.solutions.app.repositories.UserRepository;
 import com.insurance.solutions.app.resources.UserResource;
+import com.insurance.solutions.app.services.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class UserControllerTest {
+
+    String urlBase = "/users";
+
     @Autowired
     MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    private String toJson(Object o) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(o);
+    }
+
+    @Test
+    void createUser() throws Exception {
+        User user = new User("User1", "user1@mail.com", "password", UserType.BASE);
+
+        MvcResult response = mockMvc
+                .perform(
+                        post(urlBase + "/sign-up")
+                                .contentType(APPLICATION_JSON)
+                                .accept(APPLICATION_JSON)
+                                .content(toJson(user))
+                )
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").isNumber())
+                .andReturn();
+
+        User createdUser = objectMapper.readValue(response.getResponse().getContentAsString(), User.class);
+        user.setId(createdUser.getId());
+        user.setPassword(createdUser.getPassword());
+
+        assertEquals(toJson(user), toJson(userRepository.findById(createdUser.getId())));
+
+        User adminUser = new User("AdminUser1", "adminuser1@mail.com", "password", UserType.ADMIN);
+
+        MvcResult response2 = mockMvc
+                .perform(
+                        post(urlBase + "/sign-up")
+                                .contentType(APPLICATION_JSON)
+                                .accept(APPLICATION_JSON)
+                                .content(toJson(adminUser))
+                )
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").isNumber())
+                .andReturn();
+
+        User createdAdminUser = objectMapper.readValue(response2.getResponse().getContentAsString(), User.class);
+        adminUser.setId(createdAdminUser.getId());
+        adminUser.setPassword(createdAdminUser.getPassword());
+
+        assertEquals(toJson(adminUser), toJson(userRepository.findById(createdAdminUser.getId())));
+    }
 
     @Test
     public void getAll() throws Exception {
