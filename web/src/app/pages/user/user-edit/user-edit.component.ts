97515069ec951981cb5ac4776612ca446ed21849
Diego Alvarado <diegoalvarado26@hotmail.com>
2020-11-10 17:23:01 -0300

    agregada función para traer todos los usuarios menos el actual

--- web/src/app/pages/user/user-edit/user-edit.component.ts
+++ web/src/app/pages/user/user-edit/user-edit.component.ts
@@ -1,15 +1,15 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
-import {User} from "../../../../shared/models/user";
-import {UserService} from "../../../../shared/services/user.service";
-import {alreadyExistsValidator} from "../../../../shared/directives/alreadyExistsValidator.directive";
-import {Observable} from "rxjs";
-import {InsuranceCompanyService} from "../../../../shared/services/insurance-company.service";
-import {Role} from "../../../../shared/models/role";
-import {map, startWith} from "rxjs/operators";
-import {InsuranceCompany} from "../../../../shared/models/insuranceCompany";
-import {checkExistsValidator} from "../../../../shared/directives/checkExistsValidator.directive";
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {User} from '../../../../shared/models/user';
+import {UserService} from '../../../../shared/services/user.service';
+import {alreadyExistsValidator} from '../../../../shared/directives/alreadyExistsValidator.directive';
+import {Observable} from 'rxjs';
+import {InsuranceCompanyService} from '../../../../shared/services/insurance-company.service';
+import {Role} from '../../../../shared/models/role';
+import {map, startWith} from 'rxjs/operators';
+import {InsuranceCompany} from '../../../../shared/models/insuranceCompany';
+import {checkExistsValidator} from '../../../../shared/directives/checkExistsValidator.directive';
 
 @Component({
   selector: 'app-user-edit',
@@ -19,6 +19,8 @@ import {checkExistsValidator} from "../../../../shared/directives/checkExistsVal
 export class UserEditComponent implements OnInit {
   userForm: FormGroup;
   userList: User[] = [];
+  otherUsersList: User[] = [];
+
   types: Role[] = [Role.BASE, Role.ADMIN];
   typeLabels: string[] = ['Base', 'Admin'];
   filteredOptions: Observable<InsuranceCompany[]>;
@@ -34,12 +36,19 @@ export class UserEditComponent implements OnInit {
   ngOnInit(): void {
     this.getUsers();
     this.getInsuranceCompanies();
+    this.getAllOtherUsers(this.username);
   }
 
   private getUsers() {
     this.userService.users.subscribe((res) => {
       this.userList = res;
-    })
+    });
+  }
+
+  public getAllOtherUsers(username)  {
+    this.otherUsersList = [...this.userList];
+    this.otherUsersList.splice(this.userList.findIndex(
+      u => u.username === username), 1);
   }
 
   get username() {
@@ -63,7 +72,7 @@ export class UserEditComponent implements OnInit {
   }
 
   get invalid() {
-    return this.userForm.invalid
+    return this.userForm.invalid;
   }
 
   close(): void {
@@ -77,13 +86,14 @@ export class UserEditComponent implements OnInit {
 
       this.userService.save(this.data).subscribe(
         (newUser) => {
-          //el save no asigna a la compañia. Hay que hacerlo en una ruta aparte.
+          // el save no asigna a la compañia. Hay que hacerlo en una ruta aparte.
           this.userService.assignInsuranceCompany(newUser.id, this.userForm.value.insuranceCompany.id);
           this.dialogRef.close();
         },
         () => {
-          //Solo catchea el error. No hace nada mas ya que no quiero que cierre el dialogo en caso de error. El mismo service muestra la snackbar de error
-        })
+          // Solo catchea el error. No hace nada mas ya que no quiero que cierre
+          // el dialogo en caso de error. El mismo service muestra la snackbar de error
+        });
     }
   }
 
@@ -93,7 +103,7 @@ export class UserEditComponent implements OnInit {
       this.createForm();
       this.createFilteredOptions();
       this.loading = false;
-    })
+    });
 
   }
 
@@ -114,11 +124,11 @@ export class UserEditComponent implements OnInit {
         Validators.maxLength(20),
         Validators.minLength(2),
         Validators.pattern('^[a-zA-Z0-9]*$'),
-        alreadyExistsValidator(this.userList, 'username')
+        alreadyExistsValidator(this.otherUsersList, 'username')
       ]),
       password: new FormControl('', [
         Validators.minLength(8),
-        //Minimum eight characters, at least one letter, one number and one special character
+        // Minimum eight characters, at least one letter, one number and one special character
         Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).*$')
       ]),
       type: new FormControl(this.data.role, [
