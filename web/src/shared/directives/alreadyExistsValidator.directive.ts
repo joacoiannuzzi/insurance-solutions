75a1bb66d42c977f99a8be5afb77bee526ae9531
Alejo Ramirez Gismondi <28781872+Alejo-RamirezGismondi@users.noreply.github.com>
2020-11-17 17:33:57 -0300

    Added trim(), toLowerCase(), etc. to validators

--- web/src/shared/directives/alreadyExistsValidator.directive.ts
+++ web/src/shared/directives/alreadyExistsValidator.directive.ts
@@ -1,8 +1,20 @@
 import {AbstractControl, ValidatorFn} from "@angular/forms";
 
 export function alreadyExistsValidator(objects: any[], key: string): ValidatorFn {
-  return (control: AbstractControl): {[key: string]: any} | null => {
-    if(objects.find(l => control.value === l[key])) {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    if (objects.find(l =>
+      control.value
+        .toLowerCase()
+        .normalize("NFD").replace(/[\u0300-\u036f]/g, "") // Para remover las tildes
+        .trim()
+        .replace(/\s/g,'') // Para remover los espacios dentro del string
+      ===
+      l[key]
+        .toLowerCase()
+        .normalize("NFD").replace(/[\u0300-\u036f]/g, "") // Para remover las tildes
+        .trim()
+        .replace(/\s/g,'') // Para remover los espacios dentro del string
+    )) {
       return {'alreadyExistsValidator': {value: control.value}}
     }
     return null;
--- web/src/shared/directives/checkExistsValidator.directive.ts
+++ web/src/shared/directives/checkExistsValidator.directive.ts
@@ -1,9 +1,19 @@
 import {AbstractControl, ValidatorFn} from "@angular/forms";
 
 export function checkExistsValidator(objects: any[], key: string): ValidatorFn {
-  return (control: AbstractControl): {[key: string]: any} | null => {
-    if(objects.find(l => {
-      return control.value[key] === l[key];
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    if (objects.find(l => {
+      return control.value[key]
+          .toLowerCase()
+          .normalize("NFD").replace(/[\u0300-\u036f]/g, "") // Para remover las tildes
+          .trim()
+          .replace(/\s/g, '') // Para remover los espacios dentro del string
+        ===
+        l[key]
+          .toLowerCase()
+          .normalize("NFD").replace(/[\u0300-\u036f]/g, "") // Para remover las tildes
+          .trim()
+          .replace(/\s/g, '') // Para remover los espacios dentro del string
     })) {
       return null;
     }
